6b3af8b9d716032324cd85842a3ff84c7e2e04ed
Nikita Panteleev <np258@inlumine.ual.es>
2017-06-12 18:17:18 +0200

    index recive ahora los ultimos 6 posts publicados

--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from devblog.models import Post
 
 # Create your views here.
 def index(request):
-     return render(request, 'main/index.html')
+    posts = Post.objects.order_by('published_date')[:6]
+    return render(request, 'main/index.html', {'posts':posts})
